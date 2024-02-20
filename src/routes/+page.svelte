05bfaa53d671902e7792230f8a5673301665625d
Sean Cooper <scooper@adaptavist.com>
2024-02-20 09:53:16 +0000

    demo working

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import {defineCustomElements} from "@salable/stencil-library/loader";
+    defineCustomElements();
+</script>
+
+<salable-pricing-table
+        api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
+        uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
+        global-success-url="https://example.com/success"
+        global-cancel-url="https://example.com/cancel"
+        global-grantee-id="123"
+        member="456"
+></salable-pricing-table>
