6fb65b27eaf42ea917010c7be6762ac64685a4cd
Sean Cooper <scooper@adaptavist.com>
2024-02-26 14:25:09 +0000

    fix indent

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,10 @@
 </script>
 
 <salable-pricing-table
-        api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
-        uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
-        global-success-url="https://example.com/success"
-        global-cancel-url="https://example.com/cancel"
-        global-grantee-id="123"
-        member="456"
+    api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
+    uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
+    global-success-url="https://example.com/success"
+    global-cancel-url="https://example.com/cancel"
+    global-grantee-id="123"
+    member="456"
 ></salable-pricing-table>
