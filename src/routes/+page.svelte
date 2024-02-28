2d91378729949bd58b830f16aeb55d6e2623c3e9
Sean Cooper <scooper@adaptavist.com>
2024-02-28 10:34:16 +0000

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,17 @@
 <script>
     import {defineCustomElements} from "@salable/stencil-library/loader";
+
     defineCustomElements();
 </script>
 
-<salable-pricing-table
-    api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
-    uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
-    global-success-url="https://example.com/success"
-    global-cancel-url="https://example.com/cancel"
-    global-grantee-id="123"
-    member="456"
-></salable-pricing-table>
+<main class="flex min-h-screen flex-col gap-8 p-24">
+    <h1>Salable Web Components Svelte Demo</h1>
+    <salable-pricing-table
+            api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
+            uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
+            global-success-url="https://example.com/success"
+            global-cancel-url="https://example.com/cancel"
+            global-grantee-id="123"
+            member="456"
+    ></salable-pricing-table>
+</main>
