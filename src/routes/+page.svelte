bab7cbd955d80ffe82a54a7aa637ca9ef0687462
Sean Cooper <scooper@adaptavist.com>
2024-02-29 17:55:35 +0000

    fix: use test mode data

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,12 +6,22 @@
 
 <main class="flex min-h-screen flex-col gap-8 p-24">
     <h1>Salable Web Components Svelte Demo</h1>
+    <h2>Pricing Table</h2>
     <salable-pricing-table
-            api-key="s1tVhRx3WH2jUm2cWTl8g6dbNj7GJ8Zg21XuzEds"
-            uuid="0b8fc07d-6707-491a-ad6e-1828d57fe1cc"
+            api-key="test_19fda9530350e27eb3948749d511c9d51d09e7c5"
+            uuid="44363fe3-2607-437a-8b66-5375d7a762a2"
             global-success-url="https://example.com/success"
             global-cancel-url="https://example.com/cancel"
-            global-grantee-id="123"
-            member="456"
+            global-grantee-id="example-grantee-123"
+            member="example-member-123"
     ></salable-pricing-table>
+    <h2>Checkout</h2>
+    <salable-checkout
+            api-key="test_19fda9530350e27eb3948749d511c9d51d09e7c5"
+            plan-uuid='390fe44d-33d9-48fc-9863-eb641e148249'
+            member='example-member-123'
+            grantee-id='example-grantee-123'
+            success-url="https://example.com/success"
+            email="alison@example.com"
+    ></salable-checkout>
 </main>
