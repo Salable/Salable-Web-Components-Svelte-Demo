b98d922d863654d556065195b4f62d87f2035e6c
Sean Cooper <scooper@adaptavist.com>
2024-03-04 23:53:46 +0000

    use new npm packages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-    import {defineCustomElements} from "@salable/stencil-library/loader";
+    import {defineCustomElements} from "@salable/web-components/loader";
 
     defineCustomElements();
 </script>
